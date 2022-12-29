edaf908cbdb2ee769269e881444da6228b6d5588
Aaron Gustafson <aaron@easy-designs.net>
2022-12-29 15:03:03 -0800

    archive on

--- cron.sh
+++ cron.sh
@@ -2,6 +2,6 @@
 
 # build latest
 cd /Users/aarongu/Documents/GitHub/twitter.aaron-gustafson.com/ &&
-# /usr/local/bin/npm run fetch-new-data &&
+/usr/local/bin/npm run fetch-new-data &&
 /usr/local/bin/git commit -am "archive on ${date}" &&
 /usr/local/bin/git push origin main
