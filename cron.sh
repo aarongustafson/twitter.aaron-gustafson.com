3c2f9e74c2c32a62ae69938152fc89013d6fa558
Aaron Gustafson <aaron@easy-designs.net>
2022-12-29 19:47:59 -0800

    archive on Thu Dec 29 19:47:52 PST 2022

--- cron.sh
+++ cron.sh
@@ -6,9 +6,11 @@ echo "Building at ${currentDate}"
 # build latest
 cd /Users/aarongu/Documents/GitHub/twitter.aaron-gustafson.com/ &&
 /usr/local/bin/npm run fetch-new-data &&
+/usr/local/bin/git add . &&
 /usr/local/bin/git commit -am "archive on ${currentDate}" &&
 /usr/local/bin/git push origin main &&
 /usr/local/bin/npm run build &&
 cd ./_site &&
+/usr/local/bin/git add . &&
 /usr/local/bin/git commit -am "Built on ${currentDate}" &&
 /usr/local/bin/git push origin main
