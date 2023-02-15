73bd80c7efe18e2b696d4f451497330ecd168aee
Aaron Gustafson <aaron@easy-designs.net>
2023-02-14 16:04:41 -0800

    archive on Tue Feb 14 16:01:38 PST 2023

--- cron.sh
+++ cron.sh
@@ -16,13 +16,18 @@ currentDate=`date`
 echo "Building at ${currentDate}"
 
 # build latest
-cd /Users/aarongu/Documents/GitHub/twitter.aaron-gustafson.com/ &&
-/usr/local/bin/npm run fetch-new-data &&
-/usr/local/bin/git add . &&
-/usr/local/bin/git commit -am "archive on ${currentDate}" &&
-/usr/local/bin/git push origin main &&
-/usr/local/bin/npm run build &&
-cd ./_site &&
-/usr/local/bin/git add . &&
-/usr/local/bin/git commit -am "Built on ${currentDate}" &&
-/usr/local/bin/git push origin gh-pages
+cd /Users/aarongu/Documents/GitHub/twitter.aaron-gustafson.com/;
+echo "Fetching new data";
+/usr/local/bin/npm run fetch-new-data;
+echo "Done fetching, updating in Git";
+/usr/local/bin/git add .;
+/usr/local/bin/git commit -am "archive on ${currentDate}";
+/usr/local/bin/git push origin main;
+echo "Building";
+/usr/local/bin/npm run build;
+echo "Build complete, pushing to GitHub";
+cd ./_site;
+/usr/local/bin/git add .;
+/usr/local/bin/git commit -am "Built on ${currentDate}";
+/usr/local/bin/git push origin gh-pages;
+echo "All done";
